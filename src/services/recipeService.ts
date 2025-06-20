7a1c99f132eec43f25d1ff23969c8ae6457a588f
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-06-20 11:56:51 -0300

    feat: adding try catch in recipeService

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -3,26 +3,46 @@ import axios from "axios";
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1";
 
 export const fetchAllRecipes = async () => {
-  const res = await axios.get(`${BASE_URL}/search.php?s=`);
-  return res.data;
+  try {
+    const res = await axios.get(`${BASE_URL}/search.php?s=`);
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to fetch all recipes");
+  }
 };
 
 export const fetchRecipesByIngredient = async (ingredient: string) => {
-  const res = await axios.get(`${BASE_URL}/filter.php?i=${ingredient}`);
-  return res.data;
+  try {
+    const res = await axios.get(`${BASE_URL}/filter.php?i=${ingredient}`);
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to fetch recipes filtering by ingredient");
+  }
 };
 
 export const fetchRecipesByCategory = async (category: string) => {
-  const res = await axios.get(`${BASE_URL}/filter.php?c=${category}`);
-  return res.data;
+  try {
+    const res = await axios.get(`${BASE_URL}/filter.php?c=${category}`);
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to fetch recipes filtering by category");
+  }
 };
 
 export const fetchRecipesByArea = async (area: string) => {
-  const res = await axios.get(`${BASE_URL}/filter.php?a=${area}`);
-  return res.data;
+  try {
+    const res = await axios.get(`${BASE_URL}/filter.php?a=${area}`);
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to fetch recipes filtering by area");
+  }
 };
 
 export const fetchRecipeById = async (id: string) => {
-  const res = await axios.get(`${BASE_URL}/lookup.php?i=${id}`);
-  return res.data;
+  try {
+    const res = await axios.get(`${BASE_URL}/lookup.php?i=${id}`);
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to fetch recipe by id");
+  }
 };
